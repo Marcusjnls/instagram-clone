cd6a1d604c53de98243d436c81f29629410f2992
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-17 09:16:24 +0300

    Register Follow model

--- pics/admin.py
+++ pics/admin.py
@@ -4,5 +4,5 @@ from .models import Image,Profile,Comment,Follow
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Profile)
-admin.site.register(Comment)
 admin.site.register(Follow)
+admin.site.register(Comment)
