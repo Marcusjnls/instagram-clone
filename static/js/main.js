c501df9f0cf7b4c1fb23d05b5d8cffe38624f166
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-17 09:36:46 +0300

    Add custom styles file to add hoover effect and also default image for registered users

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  // $('form').submit(function(event){
+  //   event.preventDefault()
+  //   form = $("form")
+
+  //   $.ajax({
+  //     'url':'ajax/comment/',
+  //     'type':'POST',
+  //     'data':form.serialize(),
+  //     'dataType':'json',
+  //     'success': function(data){
+  //       alert(data['success'])
+  //     },
+  //   })// END of Ajax method
+  //   .done(function(data){
+  //     alert('success')
+  //   })
+  //   $('#id_comment').val('')
+    
+  }) // End of submit event
+
+}) // End of document ready function
